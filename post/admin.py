2f6771cd5b668ccd4f05d3cb1c9455ed003e2236
Gustavo-F <fgustavo016@gmail.com>
2021-06-12 21:30:39 -0300

    Created models and added in admin

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name',)
+
+
+admin.site.register(models.Category, CategoryAdmin)
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'category', 'author', 'is_published')
+    list_display_links = ('id', 'title', 'category', 'author')
+    list_editable = ('is_published', )
+
+
+admin.site.register(models.Post, PostAdmin)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'post', 'comment_date', 'is_approved')
+    list_display_links = ('id', 'author', 'post', 'comment_date')
+    list_editable = ('is_approved', )
+
+
+admin.site.register(models.Comment, CommentAdmin)
+
+
+class AprovalAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'post', 'is_approved')
+    list_display_links = ('id', 'user', 'post')
+
+
+admin.site.register(models.Aproval, AprovalAdmin)
--- post/models.py
+++ post/models.py
@@ -1,3 +1,45 @@
+from functools import partial
 from django.db import models
+from django.db.models.deletion import CASCADE, DO_NOTHING
+from django.conf import settings
+from django.db.models.fields import BLANK_CHOICE_DASH
+from django.utils import timezone
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    thumbnail = models.ImageField(
+        upload_to='posts_img/%Y/%m/%d', blank=True, null=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=CASCADE)
+    category = models.ForeignKey(Category, on_delete=DO_NOTHING)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+    is_published = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    comment = models.CharField(max_length=255)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=CASCADE)
+    post = models.OneToOneField(Post, on_delete=CASCADE)
+    comment_date = models.DateTimeField(default=timezone.now)
+    is_approved = models.BooleanField(default=True)
+
+
+class Aproval(models.Model):
+    is_approved = models.BooleanField(blank=True, null=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=CASCADE)
+    post = models.ForeignKey(Post, on_delete=CASCADE)
+
+    class Meta:
+        unique_together = (('user', 'post'),)
