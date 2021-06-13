b73fa04e0d305648673f8dbeff9d28f6e8eb4d9d
Gustavo-F <fgustavo016@gmail.com>
2021-06-12 21:28:30 -0300

    Removed app 'comment'

--- blog/settings.py
+++ blog/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'post',
-    'comment',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- blog/urls.py
+++ blog/urls.py
@@ -18,6 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('post.urls')),
-    path('comment/', include('comment.urls')),
     path('admin/', admin.site.urls),
 ]
