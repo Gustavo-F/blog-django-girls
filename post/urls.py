beaaa3ae3020d073c8de0eff801d80b4cc563b5a
Gustavo-F <fgustavo016@gmail.com>
2021-06-13 21:42:04 -0300

    Created intial views, urls and templates archives

--- post/urls.py
+++ post/urls.py
@@ -7,5 +7,7 @@ app_name = 'post'
 
 urlpatterns = [
     path('', view=views.index, name='index'),
-    path('post/<slug>', view=views.index, name='index'),
+    path('login/', view=views.login, name='login'),
+    path('register/', view=views.register, name='register'),
+    path('post_details/<slug>/', view=views.post_details, name='post_details'),
 ]
--- post/views.py
+++ post/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def index(request):
-    return render(request=request, template_name='post/index.html')
+    return render(request, 'post/index.html')
+
+
+def login(request):
+    return render(request, 'post/login.html')
+
+
+def register(request):
+    return render(request, 'post/register.html')
+
+
+def post_details(request):
+    return render(request, 'post/post_details.html')
