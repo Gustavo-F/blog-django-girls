a462454d8f2879f911fb10cb9b0515db8af5967b
Gustavo-F <fgustavo016@gmail.com>
2021-06-15 17:51:23 -0300

    Fixed logout url

--- post/urls.py
+++ post/urls.py
@@ -8,7 +8,7 @@ app_name = 'post'
 urlpatterns = [
     path('', view=views.Index.as_view(), name='index'),
     path('login/', view=views.Login.as_view(), name='login'),
-    path('logout/', view=views.logout, name='logout'),
+    path('logout/', view=views.Logout.as_view(), name='logout'),
     path('register/', view=views.Register.as_view(), name='register'),
     path('post_details/<slug>', view=views.PostDetails.as_view(), name='post_details'),
     path('write_post/', view=views.WritePost.as_view(), name='write_post'),
