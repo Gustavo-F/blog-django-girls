a72d9b4765cdde7d96db12061c340dfe4279c985
Gustavo-F <fgustavo016@gmail.com>
2022-03-15 22:40:19 -0300

    added local_settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,15 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-asm8m2d3bw-4z=7ck1ce6hi8vi45@@nx$!sr(k6#2*4*rj583n'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
-
-
 # Application definition
 
 INSTALLED_APPS = [
