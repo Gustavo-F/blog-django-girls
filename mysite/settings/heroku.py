fe5196b3318ceb4f207e5ebc1071c6cfc4110535
Gustavo Ferreira <fgustavo016@gmail.com>
2022-05-08 17:28:23 -0300

    add email configs to heroku settings

--- mysite/settings/heroku.py
+++ mysite/settings/heroku.py
@@ -20,3 +20,11 @@ AWS_S3_OBJECT_PARAMETERS = {
 }
 
 DEFAULT_FILE_STORAGE = 'blog.storage_backends.MediaStorage'  
+
+# Email Configs
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_USE_TLS = env.bool('EMAIL_USE_TLS')
+EMAIL_PORT = env.int('EMAIL_PORT')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
